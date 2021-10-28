f167c1b1d5129715e6225f6df0e41e6b9940e05a
LeoBaro <leonardo.baroncelli@inaf.it>
2021-10-28 12:36:34 +0200

    pipeline update

--- SampleProject/testing/start_test.sh
+++ SampleProject/testing/start_test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-environment=$1
-
-source environment/bin/activate
-
 pytest --cov-config=.coveragerc --junitxml=./junit_report.xml  --cov=../ test_utils.py
 
 coverage xml -o ./coverage_report.xml
